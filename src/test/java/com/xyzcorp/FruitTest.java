32ca7ad4068b081c96bdedbbffd6cf4359928bee
Leroy Nguyen <leroy.nguyen@cgi.com>
2022-02-02 16:10:08 -0500

    added new test case - banana

--- src/test/java/com/xyzcorp/FruitTest.java
+++ src/test/java/com/xyzcorp/FruitTest.java
@@ -1,6 +1,7 @@
 package com.xyzcorp;
 
 import io.restassured.http.ContentType;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -13,12 +14,32 @@ public class FruitTest {
     @Test
     public void testGetFruits() {
         given()
-            .relaxedHTTPSValidation()
+                .relaxedHTTPSValidation()
                 .accept(ContentType.JSON)
                 .when()
                 .get("https://staging.tiered-planet.net/mild-temper/fruits")
                 .then()
                 .assertThat()
                 .body("[0].description", equalTo("Winter fruit"));
+    }
+
+    @Test
+    public void testPostNewFruit() {
+        JSONObject bananaObject = new JSONObject()
+                .put("name", "banana")
+                .put("description", "a delicious treat");
+
+        System.out.println(bananaObject);
+
+        given()
+                .relaxedHTTPSValidation()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .body(bananaObject.toString())
+                .when()
+                .post("https://staging.tiered-planet.net/mild-temper/fruits")
+                .then()
+                .assertThat()
+                .statusCode(200);
     }
 }
